aab0b999eb417d887bf209bae5d8ff94407e2cc9
crossfire3001 <obiwan2035@mail.ru>
2023-10-15 13:38:53 +0500

    sending http request

--- checkout.js
+++ checkout.js
@@ -1,10 +1,23 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  'use strict';
+'use strict';
 
+(() => {
   $("#redeem").click(function () {
     let url = "http://testologia.site/promo-code?name=" + $("#promo-code").val();
     console.log(url);
+
+    let http = new XMLHttpRequest();
+    http.open("get", url);
+    http.send();
+
+    http.onreadystatechange = () => {
+     if (http.readyState === 4 && http.status === 200) {
+
+     } else {
+
+     }
+    }
   })
+
+
 
 })()
