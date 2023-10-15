7541a12b48b35f29b35d58586a64b42888ce9acd
crossfire3001 <obiwan2035@mail.ru>
2023-10-15 13:23:20 +0500

    fixed minor issues with Jquery

--- checkout.js
+++ checkout.js
@@ -1,19 +1,10 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-  'use strict'
+  'use strict';
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
-
-      form.classList.add('was-validated')
-    }, false)
+  $("#redeem").click(function () {
+    let url = "http://testologia.site/promo-code?name=" + $("#promo-code").val();
+    console.log(url);
   })
+
 })()
