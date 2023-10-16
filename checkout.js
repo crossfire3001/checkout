d40fb438ca76049097dc24f817e3c51a816c8797
crossfire3001 <obiwan2035@mail.ru>
2023-10-16 14:57:55 +0500

    checkout using Jquery AJAX

--- checkout.js
+++ checkout.js
@@ -6,27 +6,21 @@
     let url = "http://testologia.site/promo-code?name=" + promoInput.val();
 
     let http = new XMLHttpRequest();
-    http.open("get", url);
-    http.send();
     promoInput.css("border-color", "black");
 
-    http.onreadystatechange = () => {
-      if (http.readyState === 4 && http.status === 200) {
-        let result = null;
-        try {
-          result = JSON.parse(http.responseText);
-        } catch (e) {}
-
-        let promoElement = $("#promocode");
-        if (result && result.hasOwnProperty("amount")) {
-          promoElement.removeClass("d-none").addClass("d-flex");
-          promoElement.find("small").text(promoInput.val().toUpperCase());
-          promoElement.find("text-success").text("-$" + result.amount);
-        } else {
-          promoElement.removeClass("d-flex").addClass("d-none");
-          promoInput.css("border-color", "red");
-        }
+    $.ajax({
+      method: "GET",
+      url: url,
+    }).done(function (message) {
+          let promoElement = $("#promocode");
+      if (message && message.hasOwnProperty("amount")) {
+        promoElement.removeClass("d-none").addClass("d-flex");
+        promoElement.find("small").text(promoInput.val().toUpperCase());
+        promoElement.find("text-success").text("-$" + message.amount);
+      } else {
+        promoElement.removeClass("d-flex").addClass("d-none");
+        promoInput.css("border-color", "red");
       }
-    };
+    });
   });
 })();
