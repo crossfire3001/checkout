5903f905c9642eead3e8a508142785a6be48912a
crossfire3001 <obiwan2035@mail.ru>
2023-10-16 15:00:17 +0500

    added Bootstrap validation

--- checkout.js
+++ checkout.js
@@ -1,6 +1,22 @@
 "use strict";
 
 (() => {
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  });
+
   $("#redeem").click(function () {
     let promoInput = $("#promo-code");
     let url = "http://testologia.site/promo-code?name=" + promoInput.val();
